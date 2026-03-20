ec30443bf1c20f02f4c3dfc0a4698a5ddbbccafc
Ishwar Chandra Sah <424135@student.nitandhra.ac.in>
2026-03-20 22:50:41 +0530

    removed debug message from console

--- src/app/api/create-user/route.js
+++ src/app/api/create-user/route.js
@@ -48,8 +48,8 @@ export async function POST(req) {
         //     })
         // );
 
-        console.log("Region:", process.env.MY_AWS_REGION);
-        console.log("Key:", process.env.MY_AWS_ACCESS_KEY);
+        // console.log("Region:", process.env.MY_AWS_REGION);
+        // console.log("Key:", process.env.MY_AWS_ACCESS_KEY);
         await dynamo.send(
             new PutItemCommand({
                 TableName: "josaa_users_db",
