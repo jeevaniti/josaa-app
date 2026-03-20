b29e1f8fe88570ab7532cc69ef1958b4e2dfccf5
Ishwar Chandra Sah <424135@student.nitandhra.ac.in>
2026-03-20 22:15:14 +0530

    fix MY_AWS env variable names

--- src/lib/dynamo.js
+++ src/lib/dynamo.js
@@ -1,9 +1,9 @@
 import { DynamoDBClient } from "@aws-sdk/client-dynamodb";
 
 export const dynamo = new DynamoDBClient({
-    region: process.env.AWS_REGION,
+    region: process.env.MY_AWS_REGION,
     credentials: {
-        accessKeyId: process.env.AWS_ACCESS_KEY,
-        secretAccessKey: process.env.AWS_SECRET_KEY,
+        accessKeyId: process.env.MY_AWS_ACCESS_KEY,
+        secretAccessKey: process.env.MY_AWS_SECRET_KEY,
     },
 });
